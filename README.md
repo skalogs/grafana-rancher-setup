cb16962e9107c32391b1e94ec93daaa2448ec266
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2017-09-21 18:39:09 +0200

    kickstart

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # grafana-rancher-setup
+
+This utility container is here to push specific configuration to grafana from [rancher-compose.yml's metadata](rancher-metadata/rancher-compose.yml):
+ - datasources
+ - dashboards
+ - notifications
+
+This elements rely on a custom structure supporting multiple entries (a structured json array):
+```json
+[
+  {
+    "name": "mytemplate",
+    "value": #Paste your template or repository config here
+  }
+]
+```
+
+All these configurations option are optional.
---
+++ startup.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+set -x
+
+RANCHER_BASEURL=rancher-metadata.rancher.internal/latest
+
+if [ -z "${SERVICE_GRAFANA_USERNAME}"]; then
+  GRAFANA_AUTH=""
+else
+  GRAFANA_AUTH="${SERVICE_GRAFANA_USERNAME}:${SERVICE_GRAFANA_PASSWORD}@"
+fi
+GRAFANA_URL=http://${GRAFANA_AUTH}${SERVICE_GRAFANA_HOST}:${SERVICE_GRAFANA_PORT}
+
+function checkGrafana {
+    a="`curl ${GRAFANA_URL}/api/metrics &> /dev/null; echo $?`"
+    while  [ $a -ne 0 ];
+    do
+        a="`curl ${GRAFANA_URL}/api/metrics &> /dev/null; echo $?`"
+        sleep 1
+    done
+}
+
+function retryHttp {
+    url=$1
+    content=$2
+    curlCommand="curl ${url} --compressed -H 'Content-Type: application/json;charset=UTF-8' --write-out %{http_code} --output /dev/null -d @${content}"
+    status=$(curl ${url} --compressed -H 'Content-Type: application/json;charset=UTF-8' --write-out %{http_code} --output /dev/null -d @${content})
+    while  [ $status -ne 200 ] && [ $status -ne 409 ] ;
+    do
+        status=$(curlCommand)
+        sleep 1
+    done
+}
+
+checkGrafana
+
+mkdir /grafana
+
+#get datasources
+response=$(curl --write-out %{http_code} --silent --output /dev/null http://${RANCHER_BASEURL}/self/service/metadata/datasources)
+if [ "$response" -eq 200 ]
+then
+    curl http://${RANCHER_BASEURL}/self/service/metadata/datasources > /grafana/datasources.json
+    mkdir -p /grafana/datasources
+    jq -rc '.[]' /grafana/datasources.json | while IFS='' read objectConfig ; do
+      name=$(echo $objectConfig | jq -r .name)
+      config=$(echo $objectConfig | jq .value)
+      if [ "$name" = "null"] && [ "$config" = "null"]; then
+        echo "datasource name or config is null, ignoring this entry..."
+      else
+        echo Posting datasource config $name
+        echo $config > /grafana/datasources/$name.json
+        retryHttp ${GRAFANA_URL}/api/datasources/ /grafana/datasources/$name.json
+      fi
+    done
+fi
+
+#get dashboards
+response=$(curl --write-out %{http_code} --silent --output /dev/null http://${RANCHER_BASEURL}/self/service/metadata/dashboards)
+if [ "$response" -eq 200 ]
+then
+    curl http://${RANCHER_BASEURL}/self/service/metadata/dashboards > /grafana/dashboards.json
+    mkdir -p /grafana/dashboards
+    jq -rc '.[]' /grafana/dashboards.json | while IFS='' read objectConfig ; do
+      name=$(echo $objectConfig | jq -r .name)
+      config=$(echo $objectConfig | jq .value)
+      if [ "$name" = "null"] && [ "$config" = "null"]; then
+        echo "dashboards name or config is null, ignoring this entry..."
+      else
+        echo Posting dashboards config $name
+        echo $config > /grafana/dashboards/$name.json
+        retryHttp ${GRAFANA_URL}/api/dashboards/db /grafana/dashboards/$name.json
+      fi
+    done
+fi
+
+#get notifications
+response=$(curl --write-out %{http_code} --silent --output /dev/null http://${RANCHER_BASEURL}/self/service/metadata/notifications)
+if [ "$response" -eq 200 ]
+then
+    curl http://${RANCHER_BASEURL}/self/service/metadata/notifications > /grafana/notifications.json
+    mkdir -p /grafana/notifications
+    jq -rc '.[]' /grafana/notifications.json | while IFS='' read objectConfig ; do
+      name=$(echo $objectConfig | jq -r .name)
+      config=$(echo $objectConfig | jq .value)
+      if [ "$name" = "null"] && [ "$config" = "null"]; then
+        echo "notifications name or config is null, ignoring this entry..."
+      else
+        echo Posting notifications config $name
+        echo $config > /grafana/notifications/$name.json
+        retryHttp ${GRAFANA_URL}/api/alert-notifications /grafana/notifications/$name.json
+      fi
+    done
+fi
